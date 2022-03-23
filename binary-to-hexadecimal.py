54a8c1a3b1d3e43ab804784a385c5f4fd744e1fe
Srishty Mangutte <68679980+Srish-ty@users.noreply.github.com>
2022-03-23 19:47:06 +0530

    Update binary-to-hexadecimal.py

--- binary-to-hexadecimal.py
+++ binary-to-hexadecimal.py
@@ -1,14 +1,25 @@
-line = str(input())
+#write ur binary code without any spaces
+#this will convert it to hexadecimal
+
+
+line = str(input)
 lis=[line[i:i+4] for i in range(0, len(line), 4)]
 
 
-res=0
+hexal="ABCDEF"
+res=""
 
 for li in lis:
-    res*=10
+    j=0
     for i in range(4):
-        res+=int(li[i])*(2**(3-i))
-    
+        j+=int(li[i])*(2**(3-i))
+    #print(j)
+    if j<10:
+        res+=str(j)
+    else:
+        res+=hexal[j-10]
+
     
 print(res)   
+         
         
