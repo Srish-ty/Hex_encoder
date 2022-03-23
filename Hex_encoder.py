57847c9c3a4d7db4c95c98cd0a2474fbe73fa38e
Srishty Mangutte <68679980+Srish-ty@users.noreply.github.com>
2022-03-23 19:49:07 +0530

    Create Hex_encoder.py

---
+++ Hex_encoder.py
@@ -0,0 +1,51 @@
+#ASCII to binary
+chara=input()
+bina=""
+for c in chara:
+    ascival=ord(c)
+    #print(ascival," ")
+    bin=0
+    for i in range(9):
+        bin+=ascival%2
+        bin/=10
+        ascival=ascival//2
+    bin*=1000000000
+    bin+=0.001
+    bin=int(bin)
+    leng=len(str(bin))
+    if leng<8:
+        bina+=("0"*(8-leng)+str(bin))
+    else:
+        bina+=str(bin)
+    
+#prints binary code    
+#print(bina)
+
+#let's convert that binary into hexadecimal
+
+
+line = bina
+lis=[line[i:i+4] for i in range(0, len(line), 4)]
+
+
+hexal="ABCDEF"
+res=""
+
+for li in lis:
+    j=0
+    for i in range(4):
+        j+=int(li[i])*(2**(3-i))
+    #print(j)
+    if j<10:
+        res+=str(j)
+    else:
+        res+=hexal[j-10]
+
+    
+print(res)   
+<<<<<<< HEAD:hex_encoder.py
+           
+        
+=======
+        
+>>>>>>> db6eac66ef9f963be7af870560fd64c9fb88b547:Hex_encoder.py
